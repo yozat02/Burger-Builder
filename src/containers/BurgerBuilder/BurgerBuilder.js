e5bf4468c09b53cd42280f52b1a6a08553f5851d
youssefziat02 <youssefziat02@gmail.com>
2020-08-24 15:44:18 +0100

    Add dynamic ingrendients to burger

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,11 +3,19 @@ import Auxi from '../../HOC/Auxi';
 import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends Component {
+    state = {
+        ingredients : {
+            salad :1,
+            bacon :1,
+            meat:2,
+            cheese:2
+        }
+    }
     render(){
         return (
             <Auxi>
                 <div>
-                    <Burger />
+                    <Burger ingredients={this.state.ingredients}/>
                 </div>
                 <div>
                     Burger Controls
